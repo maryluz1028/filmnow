384e170b129aed39855692730e8f2f6ee99c9f9f
maryluz1028 <parionamaryluz@gmail.com>
2022-08-31 11:09:21 -0500

    add footer

--- footer.php
+++ footer.php
@@ -1,3 +1,7 @@
 </main>
-<footer></footer>
+<footer>
+    <div class="container">
+        <div class="py-8 flex justify-center"><span>Copyright ©2022 — Mary Pariona</span></div>
+    </div>
+</footer>
 <?php get_template_part( 'templates/partials/document-close' );
